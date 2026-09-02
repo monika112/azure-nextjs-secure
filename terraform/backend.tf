cbd3d1713aa9ba019a9d410d156d34be1361c009
Monika112 <mehrotra.monika@gmail.com>
2026-09-02 11:56:38 -0400

    moved tfstate storage to new resource group

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-nextweb-prod"
+    resource_group_name  = "rg-nextweb-tfstate"
     storage_account_name = "stnextwebprod"
     container_name       = "tfstate"
     key                  = "nextjs/prod.tfstate"
